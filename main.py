391e54447e9a55c769b228480c329a44c69c8e3b
Rubin <mcribling@gmail.com>
2021-06-22 12:22:15 -0500

    start of snake game

--- main.py
+++ main.py
@@ -2,20 +2,35 @@ import pygame
 from pygame.constants import KEYDOWN, K_DOWN, K_LEFT, K_RIGHT, K_UP
 from pygame.locals import *
 
+class Snake:
+    def __init__(self):
+        self.block = pygame.image.load("resources/block.jpg").convert()
+        self.x = 100
+        self.y = 100
+ 
+    def draw(self):
+        surface.blit(block,(block_x,block_y))
+        
+class Game:
+    def__init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((500,500))
+        self.surface.fill((255,255,255))
+
+
+def run(self):
+    
+    pass
+
 def draw_block():
     surface.fill((110, 110, 5))
     surface.blit(block, (block_x, block_y))
     pygame.display.flip()
 
 if __name__ == "__main__":
-    pygame.init()
-
-    surface = pygame.display.set_mode((500,500))
-    surface.fill((255,255,255))
-
-    block = pygame.image.load("resources/block.jpg").convert()
-    block_x = 100
-    block_y = 100
+    game = Game()
+    game.run()
+    
     surface.blit(block,(block_x,block_y))
 
     pygame.display.flip( )
