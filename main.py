c83987d9d2f29a7e3adeb3e5f533ceed342ae547
Rubin <mcribling@gmail.com>
2021-06-24 13:20:45 -0500

    added apple and snake now has a length that gets added on

--- main.py
+++ main.py
@@ -1,73 +1,107 @@
 import pygame
 from pygame.constants import KEYDOWN, K_DOWN, K_LEFT, K_RIGHT, K_UP
 from pygame.locals import *
+import time
+
+SIZE = 40
+
+class Apple:
+    def __init__(self, parent_screen):
+        self.image = pygame.image.load("resources/apple.jpg").convert()
+        self.parent_screen = parent_screen
+        self.x = SIZE*3
+        self.y = SIZE*3
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
 
 class Snake:
-    def __init__(self, parent_screen):
+    def __init__(self, parent_screen, length):
+        self.length = length
+        self.parent_screen = parent_screen
         self.block = pygame.image.load("resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
- 
+        self.x = [SIZE]*length
+        self.y = [SIZE]*length
+        self.direction = 'down'
+    
     def draw(self):
-        self.surface.fill((110, 110, 5))
-        self.parent_screen.blit(self.block,(self.x,self.y))
+        self.parent_screen.fill((110, 110, 5))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
     def move_left(self):
-        self.x -= 10
-        self.draw() 
+       self.direction = 'left'
 
     def move_right(self):
-        self.x += 10
-        self.draw() 
+        self.direction = 'right'
 
     def move_up(self):
-        self.y -= 10
-        self.draw() 
+        sself.direction = 'up'
 
     def move_down(self):
-        self.y += 10
-        self.draw() 
-        
-class Game:
-    def__init__(self)
-    pygame.init()
-    self.surface = pygame.display.set_mode((500,500))
-    self.surface.fill((255,255,255))
-    self.snake = snake(self.surface)
-    self.snake.draw()
-
-def run(self):
+        self.direction = 'down'
     
-    pass
+    def walk(self):
 
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
 
-if __name__ == "__main__":
+        if self.direction == 'left':
+            self.x[0] -= SIZE
+        if self.direction == 'right':
+            self.x[0] += SIZE
+        if self.direction == 'up':
+            self.y[0] -= SIZE
+        if self.direction == 'down':
+            self.y[0] += SIZE
+        
+        self.draw()
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000,800))
+        self.surface.fill((255,255,255))
+        self.snake = Snake(self.surface, 2)
+        self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+
+    def run(self):
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+                
+                    if event.key == K_UP:
+                        self.snake.move_left()
+
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+    
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+
+                elif event.type == QUIT:
+                    running = False
+
+        self.play()
+        
+        time.sleep(0.3)
+      
+if __name__ == '__main__':
     game = Game()
     game.run()
-    
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-                
-                if event.key == K_UP:
-                   self.snake.move_left()
-
-                if event.key == K_DOWN:
-                    self.snake.move_down()
-
-                if event.key == K_LEFT:
-                    self.snake.move_left()
-    
-                if event.key == K_RIGHT:
-                   self.snake.move_right()
-
-
-
-            elif event.type == QUIT:
-                running = false
-      
