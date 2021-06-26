e074039ccf9b15cbdbd0482120efa1fdc7f4bea6
Rubin <mcribling@gmail.com>
2021-06-26 01:31:57 -0500

    added apple and some game logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
-from pygame.constants import KEYDOWN, K_DOWN, K_LEFT, K_RIGHT, K_UP
 from pygame.locals import *
 import time
+import random
 
 SIZE = 40
 
@@ -16,6 +16,11 @@ class Apple:
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
+    def move(self):
+        self.x = random.randint(0,24) * SIZE
+        self.y = random.randint(0,19) * SIZE
+
+
 class Snake:
     def __init__(self, parent_screen, length):
         self.length = length
@@ -24,6 +29,11 @@ class Snake:
         self.x = [SIZE]*length
         self.y = [SIZE]*length
         self.direction = 'down'
+
+    def increase_length(self):
+        self.length+=1
+        self.x.append(-1)
+        self.y.append(-1)
     
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
@@ -38,7 +48,7 @@ class Snake:
         self.direction = 'right'
 
     def move_up(self):
-        sself.direction = 'up'
+        self.direction = 'up'
 
     def move_down(self):
         self.direction = 'down'
@@ -46,8 +56,8 @@ class Snake:
     def walk(self):
 
         for i in range(self.length-1,0,-1):
-            self.x[i] = self.x[i - 1]
-            self.y[i] = self.y[i - 1]
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
 
         if self.direction == 'left':
             self.x[0] -= SIZE
@@ -70,9 +80,20 @@ class Game:
         self.apple = Apple(self.surface)
         self.apple.draw()
 
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+
+        return False
+
     def play(self):
         self.snake.walk()
         self.apple.draw()
+
+        if self.is_collision(self.snake.x[0], self.snake[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move
 
     def run(self):
         running = True
@@ -84,7 +105,7 @@ class Game:
                         running = False
                 
                     if event.key == K_UP:
-                        self.snake.move_left()
+                        self.snake.move_up()
 
                     if event.key == K_DOWN:
                         self.snake.move_down()
