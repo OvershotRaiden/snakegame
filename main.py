0496fde29cf56c77432e3fa501125625d3c24a8a
Rubin <mcribling@gmail.com>
2021-06-28 12:18:52 -0500

    added apple movement and the snake gains length when the apple is eaten

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ class Apple:
         pygame.display.flip()
 
     def move(self):
-        self.x = random.randint(1,25)*SIZE
-        self.y = random.randint(1,20)*SIZE
+        self.x = random.randint(1,24)*SIZE
+        self.y = random.randint(1,19)*SIZE
 
 class Snake:
     def __init__(self, parent_screen, length):
@@ -30,6 +30,11 @@ class Snake:
         self.length = length
         self.x = [40]*length
         self.y = [40]*length
+    
+    def increase_length(self):
+        self.length+=1
+        self.x.append(-1)
+        self.y.append(-1)
 
     def move_left(self):
         self.direction = 'left'
@@ -68,7 +73,7 @@ class Snake:
         pygame.display.flip()
 
     def increase_length(self):
-        self.length += 1
+        self.length+=1
         self.x.append(-1)
         self.y.append(-1)
 
@@ -76,14 +81,14 @@ class Game:
     def __init__(self):
         pygame.init()
         self.surface = pygame.display.set_mode((1000, 800))
-        self.snake = Snake(self.surface, 5)
+        self.snake = Snake(self.surface, 2)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
 
     def is_collision(self, x1, y1, x2, y2):
-        if x1 >= x2 and x1 <= x2 + SIZE:
-            if y1 >= y2 and y1 <= y2 + SIZE:
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
                 return True
 
 
@@ -92,8 +97,8 @@ class Game:
         self.apple.draw()
 
         if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
-            print('collision occurred')
-
+            self.snake.increase_length()
+            self.apple.move()
 
     def run(self):
         running = True
